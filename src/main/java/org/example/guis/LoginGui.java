e7769ff4c510ecf1a01ac031cd410eec39ab2bd7
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-01-08 15:15:48 -0300

    login gui already made

--- src/main/java/org/example/guis/LoginGui.java
+++ src/main/java/org/example/guis/LoginGui.java
@@ -1,5 +1,8 @@
 package org.example.guis;
 
+import javax.swing.*;
+import java.awt.*;
+
 /*
     This gui will allow user to login or launch the register gui
     This extends from the BaseFrame which emans we will need to define our own addGuiComponents()
@@ -11,6 +14,60 @@ public class LoginGui extends BaseFrame{
 
     @Override
     protected void addGuiComponentes() {
+        // create banking app label
+        JLabel bankingAppLabel = new JLabel("Banking Application");
 
+        // set the location and the size of the gui component
+        bankingAppLabel.setBounds(0, 20, super.getWidth(), 40);
+
+        // change the font style
+        bankingAppLabel.setFont(new Font("Dialog", Font.BOLD, 32));
+
+        // center text in JLabel
+        bankingAppLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // add to gui
+        add(bankingAppLabel);
+
+        // username label
+        JLabel usernameLabel = new JLabel("Username");
+
+        // getWidth() returns us the width of our frame which is about 420
+        usernameLabel.setBounds(20, 120, getWidth() -30, 24);
+        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(usernameLabel);
+
+        // create username field
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(20, 160, getWidth() -50, 40);
+        usernameField.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(usernameField);
+
+        // password label
+        JLabel passwordLabel = new JLabel("Password");
+
+        // getWidth() returns us the width of our frame which is about 420
+        passwordLabel.setBounds(20, 280, getWidth() -50, 24);
+        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(passwordLabel);
+
+        // crate password field
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(20, 320, getWidth() -50, 40);
+        passwordField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(passwordField);
+
+        // create login button
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(20, 460, getWidth() -50, 40);
+        loginButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        add(loginButton);
+
+        // create register label
+        JLabel registerLabel = new JLabel("<html><a href=\"#\">Don't have an account? Register Here</a></html>");
+        registerLabel.setBounds(0, 510, getWidth() -10, 30);
+        registerLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(registerLabel);
     }
 }
